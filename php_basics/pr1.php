5593ac8e1d5893edb6073a9eb0cef3104e4ea5af
Deep Patel <deeppatel05082005@gmail.com>
2026-01-29 14:11:51 +0530

    PHP-001 - Variables: Defined integers, strings, booleans and arrays

---
+++ php_basics/pr1.php
@@ -0,0 +1,18 @@
+<?php
+$hello=20;
+echo $hello;
+echo "hey hi are you..";
+echo $hello."hello";
+
+$isActive = true;
+echo $isActive;
+var_dump($isActive);
+//arrays
+
+$fruits=["apple","amla","grapes"];
+$length=count($fruits);
+for($i=0;$i<$length;$i++)
+{
+     echo $fruits[$i] ;
+}
+?>
