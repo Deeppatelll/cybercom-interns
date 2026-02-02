14499d5ef8d39d7a5740761736d572a2d99adacb
Deeppatelll <deeppatel05082005@gmail.com>
2026-02-02 10:48:40 +0530

    PHP-008 - Getters and Setters (Encapsulation)

--- Php-basics/pr4.php
+++ Php-basics/pr4.php
@@ -13,5 +13,38 @@ class Employee {
 $emp = new Employee("Deep", 45000);
 
 echo "Name: " . $emp->name;
+//task 2 
+
+class Employee {
+    public $name;
+    private $salary;
+
+    public function __construct($name, $salary) {
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+    public function getSalary() {
+        return $this->salary;
+    }
+
+    public function setSalary($amount) {
+        if ($amount > 0) {
+            $this->salary = $amount;
+        } else {
+            echo "Invalid salary amount.";
+        }
+    }
+}
+
+$emp = new Employee("Deep", 40000);
+
+echo "Salary: " . $emp->getSalary() . "<br>";
+
+$emp->setSalary(50000);
+
+echo "Updated Salary: " . $emp->getSalary();
+
+
 
 ?>
