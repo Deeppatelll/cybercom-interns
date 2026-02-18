ec76e8decca122b4db9d1043705b910c4d50db07
Deeppatelll <deeppatel05082005@gmail.com>
2026-02-18 18:24:21 +0530

    needed changes

--- mvc/app/code/Catalog/Controllers/Product.php
+++
@@ -1,6 +0,0 @@
-<?php 
-class Catalog_Controllers_Product
-{
-     public function listAction() { echo "list action"; }
-     public function viewAction() { echo "view action"; }
-} ?>
--- mvc/index.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-require_once __DIR__ . '/app/Sdp.php';
-require_once __DIR__ . '/app/code/autoload.php';
-Sdp::run();
-?>
